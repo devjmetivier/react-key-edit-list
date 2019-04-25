2c4efca87383d30d41682305c7fc2d406f8ada8e
Devin Metivier <devinmetivier@gmail.com>
2019-04-25 09:20:25 -0400

    testing out props of DragDropContext

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ import Column from './components/Column';
 const App = () => {
   const [columnState, updateColumnState] = useState(initialData);
 
+  const onDragStart = () => {
+    document.body.style.color = `orange`;
+    document.body.style.transition = `background .2s ease`;
+  };
+
+  const onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(columnState.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  };
+
   const onDragEnd = result => {
+    document.body.style.color = `inherit`;
+    document.body.style.backgroundColor = `inherit`;
     const { destination, source, draggableId } = result;
 
     if (!destination) return;
@@ -41,7 +56,11 @@ const App = () => {
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
       {columnState.columnOrder.map(columnId => {
         const column = columnState.columns[columnId];
         const tasks = column.taskIds.map(taskId => columnState.tasks[taskId]);
@@ -52,4 +71,4 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById(`root`));
