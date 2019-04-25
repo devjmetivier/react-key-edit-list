eff79715512fe63103d422e982e88cd4f3e10e05
Devin Metivier <devinmetivier@gmail.com>
2019-04-25 09:24:10 -0400

    testing out drag handle

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,11 +3,20 @@ import styled from 'styled-components';
 import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
+  display: flex;
   margin-bottom: 8px;
   padding: 8px;
   border: 1px solid lightgray;
   border-radius: 2px;
   background: ${props => (props.isDragging ? `lightgreen` : `white`)};
+`;
+
+const Handle = styled.div`
+  margin-right: 8px;
+  width: 20px;
+  height: 20px;
+  background: orange;
+  border-radius: 4px;
 `;
 
 const Task = props => {
@@ -19,9 +28,9 @@ const Task = props => {
         <Container
           ref={provided.innerRef}
           {...provided.draggableProps}
-          {...provided.dragHandleProps}
           isDragging={snapshot.isDragging}
         >
+          <Handle {...provided.dragHandleProps} />
           {task.content}
           {provided.placeholder}
         </Container>
