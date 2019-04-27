67feeb49b35e9eae42c3b2e15ff6f72d3f96fe01
Devin Metivier <devinmetivier@gmail.com>
2019-04-26 23:47:04 -0400

    made data view package agnostic

--- src/components/DataView.js
+++ src/components/DataView.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
 
-const Pre = styled.pre`
-  margin: 0 0 0 30px;
-  width: 500px;
-  height: 500px;
-  border: 1px solid lightgray;
-  border-radius: 2px;
-`;
+const containerStyles = {
+  position: 'fixed',
+  top: '0',
+  right: '0',
+  height: '100vh',
+};
+
+const buttonStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'absolute',
+  right: 'calc(100% + 15px)',
+  top: '15px',
+  width: '40px',
+  height: '40px',
+};
+
+const preStyles = {
+  margin: '0',
+  padding: '8px',
+  width: '100%',
+  maxWidth: '100%',
+  height: '100%',
+  border: '1px solid lightgray',
+  borderRadius: '2px',
+  background: 'white',
+};
 
 // eslint-disable-next-line
-const DataView = props => <Pre>{JSON.stringify(props.data, null, props.tabs ? `\t` : 2)}</Pre>;
+const DataView = props =>{
+  const { data, tabs } = props;
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div style={{ ...containerStyles, width: visible ? '400px' : '0' }}>
+      <button
+        type='button'
+        style={buttonStyles}
+        onClick={() => setVisible(prev => !prev)}
+      >
+        i
+      </button>
+      {visible && (
+        <pre style={{ ...preStyles, display: visible ? 'block' : 'none' }}>
+          {JSON.stringify(data, null, tabs ? `\t` : 2)}
+        </pre>
+      )}
+    </div>
+  );
+};
 
 export default DataView;
